057dbdfc641b38c8a21cca7d6c6075f343e130c8
jairus-m <114552516+jairus-m@users.noreply.github.com>
2025-06-24 10:05:55 -0700

    Simplify main entry point function

--- assignments/movie-sentiment/src/main.py
+++ assignments/movie-sentiment/src/main.py
@@ -1,26 +1,22 @@
 import subprocess
-from src.utils import(
-    setup_logging,
-    download_kaggle_dataset,
-    DATA_PATH,
-    MODEL_PATH
-)
+from src.utils import setup_logging, MODEL_PATH
 
 logger = setup_logging()
 
+
 def main():
     """Main entry point for the movie sentiment analysis application."""
-    if not MODEL_PATH.exists() and DATA_PATH.exists():
-        logger.info("Training model since model file doesn't exist and IMDB data is present...")
-        subprocess.run(["python", "-m", "src.train.train_model"])
-    elif MODEL_PATH.exists():
+    if MODEL_PATH.exists():
         logger.info("Model file already exists, skipping training...")
     else:
+        logger.info("Model file not found.", "Training model...")
+        # Note: train_model.py handles the download of the IMDB data if it doesn't exist
         subprocess.run(["python", "-m", "src.train.train_model"])
 
     # Run Streamlit app
     logger.info("Starting Streamlit app...")
     subprocess.run(["streamlit", "run", "src/streamlit/app.py"])
+
 
 if __name__ == "__main__":
     main()
