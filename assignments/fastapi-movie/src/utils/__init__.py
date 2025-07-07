c394ee4b05ea58fbe0f7814a0f6a67bc88b0f95d
jairus-m <114552516+jairus-m@users.noreply.github.com>
2025-07-07 07:02:07 -0700

    Add middleware for request/response logging

---
+++ assignments/fastapi-movie/src/utils/__init__.py
@@ -0,0 +1,22 @@
+from .constants import (
+    DATA_PATH,
+    MODEL_PATH,
+    KAGGLE_DATASET_PATH,
+    KAGGLE_DATASET_NAME,
+    LOG_PATH,
+)
+from .logger import logger
+from .kaggle import download_kaggle_dataset
+from .middleware import log_middleware_request, log_middleware_response
+
+__all__ = [
+    "DATA_PATH",
+    "MODEL_PATH",
+    "LOG_PATH",
+    "KAGGLE_DATASET_PATH",
+    "KAGGLE_DATASET_NAME",
+    "logger",
+    "download_kaggle_dataset",
+    "log_middleware_request",
+    "log_middleware_response",
+]
